47305fbf1fb5a4e542b2ccd47699ae09fe34eeaf
ironzombi <span@ironzombi.com>
2021-06-30 18:43:37 +0800

    basic dns lookup tool

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func getIP(fqdn string) []net.IP {
@@ -36,14 +37,37 @@ func checkInput(checkstr string) string {
 }
 
 func cmdMode() {
-
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("#>: ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("something went wrong getting input")
+		}
+		text = strings.Replace(text, "\n", "", -1)
+		if text == "exit" {
+			os.Exit(1)
+		}
+		addr := net.ParseIP(text)
+		if addr != nil {
+			result, _ := net.LookupAddr(text)
+			fmt.Printf("%s\n", result)
+		} else {
+			result, _ := net.LookupIP(text)
+			fmt.Printf("%s\n", result)
+		}
+	}
 }
 
 func main() {
+	// check if any arguments passed
 	if len(os.Args[1:]) == 0 {
+		// no args = cmdMode
 		cmdMode()
 	}
+	// check what the argument is
 	argument := os.Args[1]
+	// is arg is a file, resolve each line
 	if _, err := os.Stat(argument); err == nil {
 		file, err := os.Open(argument)
 		if err != nil {
@@ -54,7 +78,7 @@ func main() {
 		for scanner.Scan() {
 			fmt.Println(getIP(scanner.Text()))
 		}
-
+		// if arg is not a file, check it and resolve it.
 	} else if os.IsNotExist(err) {
 		fmt.Println(checkInput(argument))
 
